0e469c4d87fc3da85b70171e1c52c53b4f8526ea
SaberHardy <samirsamado5@gmail.com>
2024-03-06 20:47:50 -0500

    login/register template

--- members/forms.py
+++ members/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -28,3 +28,29 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'Repeat Password'
 
+
+class EditProfileForm(UserChangeForm):
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control',
+                   'placeholder': 'Type your email here'}))
+
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Type your first name'}))
+
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Type your last name'}))
+
+    username = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Type your last name'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop('password')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
--- members/urls.py
+++ members/urls.py
@@ -3,6 +3,7 @@ from members import views
 
 urlpatterns = [
   path('register/', views.UserRegisterView.as_view(), name='register'),
+  path('edit_profile/', views.UpdateProfileView.as_view(), name='edit_profile'),
   # path('login/', views.UserRegisterView.as_view(), name='login'),
 ]
 
